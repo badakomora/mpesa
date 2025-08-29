5129f0e5c0aea85de516dc330a3e9536dadabb6b
badakomora <badakomora06@gmail.com>
2025-08-28 20:45:25 -0700

    port

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import router from "./routes/lipaRoute";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 //middlewares
 app.use(express.json());
